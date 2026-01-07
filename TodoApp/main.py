a96ff3e2cf9f67470753d73c7013be227d448d9e
Facundo Enriquez <fenriquez@inym.org.ar>
2026-01-07 11:11:32 -0300

    separando archivos en routers

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,37 +1,12 @@
-from fastapi import Depends, FastAPI, HTTPException, Path
-from typing import Annotated, TypeAlias
-from starlette import status
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
 import models
-from models import Todos
-from database import SessionLocal, engine
-
+from database import engine
+from routers import auth, todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-db_dependency: TypeAlias = Annotated[Session, Depends(get_db)]
-
-
-@app.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
-
-
-@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if todo_model is not None:
-        return todo_model
-    raise HTTPException(status_code=404, detail="Todo not found")
+app.include_router(auth.router)
+app.include_router(todos.router)
